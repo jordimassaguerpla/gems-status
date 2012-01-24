367ac7795a67e3e3a858d2c4007a3c1afa427cef
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2012-01-24 13:10:35 +0100

    Move to bin directory

--- lib/gems-status.rb
+++ lib/gems-status.rb
@@ -2,7 +2,6 @@
 #TODO: implement license checker: ninka??
 #TODO: do we need other checkers?
 #TODO: implement a nicer interface
-#TODO: move this to bin directory
 #TODO: implement tests (need refactoring on getting the data)
  
 require "rubygems"
@@ -16,13 +15,21 @@ require "lockfilegems"
 require "rubygemsgems"
 require "gemscompositecommand"
 
-conf = YAML::load(File::open("../conf/test.yaml"))
-gems_composite_command = GemsCompositeCommand.new
-conf.each do |c| 
-  gems = eval(c["classname"]).new(c)
-  gems_composite_command.add_command(gems)
-end
-gems_composite_command.execute
-gems_composite_command.print
+class GemStatus
+  def initialize(conf_file)
+    @conf_file = conf_file
+    @conf = YAML::load(File::open(conf_file))
+  end
 
+  def execute
+    gems_composite_command = GemsCompositeCommand.new
+    @conf.each do |c| 
+      gems = eval(c["classname"]).new(c)
+      gems_composite_command.add_command(gems)
+    end
+    gems_composite_command.execute
+    gems_composite_command.print
+  end
+
+end
 
