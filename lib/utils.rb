7c286e2c8ab9b28cbc7afd9d505b0de3c5041357
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2012-02-03 17:27:22 +0100

    test utils

--- lib/utils.rb
+++ lib/utils.rb
@@ -1,17 +1,14 @@
 class Utils
   def Utils.check_parameters(classname, conf, parameters)
     if !conf['classname'] then
-      $stderr.puts "ERROR: trying to initialize #{classname} when parameter classname does not exists"
-      exit
+      raise "ERROR: trying to initialize #{classname} when parameter classname does not exists"
     end
     if conf['classname'] != classname then
-      $stderr.puts "ERROR: trying to initialize #{classname} when parameter classname is #{conf['classname']}"
-      exit
+      raise "ERROR: trying to initialize #{classname} when parameter classname is #{conf['classname']}"
     end
     parameters.each do |p|
       if !conf[p] then
-        $stderr.puts "ERROR: parameter #{p} not found for #{classname}"
-        exit
+        raise "ERROR: parameter #{p} not found for #{classname}"
       end
     end
   end
---
+++ test/test-utils.rb
@@ -0,0 +1,51 @@
+$:.unshift File.join(File.dirname(__FILE__), "..", "lib")
+require 'test/unit'
+require 'rubygems'
+require 'utils'
+
+class TestUtils < Test::Unit::TestCase
+  def test_check_parameters_no_classname
+    begin
+      Utils::check_parameters("classname", {}, [])
+      assert(false)
+    rescue RuntimeError
+      assert(true)
+    end
+  end
+  def test_check_parameters_different_class_name
+    begin
+      Utils::check_parameters("classname", { "classname" => "wrong_classname" },
+                              [])
+      assert(false)
+    rescue RuntimeError
+      assert(true)
+    end
+  end
+  def test_check_parameters_equal_class_name
+    begin
+      Utils::check_parameters("classname", { "classname" => "classname" }, 
+                              [])
+      assert(true)
+    rescue RuntimeError
+      assert(false)
+    end
+  end
+  def test_check_parameters_wrong_parameters
+    begin
+      Utils::check_parameters("classname", { "classname" => "classname" }, 
+                              [ "parameter" ])
+      assert(false)
+    rescue RuntimeError
+      assert(true)
+    end
+  end
+  def test_check_parameters_rigth_parameters
+    begin
+      Utils::check_parameters("classname", { "classname" => "classname", 
+                              "parameter" => "value" }, [ "parameter" ])
+      assert(true)
+    rescue RuntimeError
+      assert(false)
+    end
+  end
+end
