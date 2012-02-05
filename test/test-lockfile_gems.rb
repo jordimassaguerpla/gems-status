f27864a6176dc3cf0a592d4b20a2e879153fa52b
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2012-02-05 18:14:03 +0100

    add dependencies tests

--- test/test-lockfile_gems.rb
+++ test/test-lockfile_gems.rb
@@ -16,10 +16,15 @@ class TestLockfileGems < Test::Unit::TestCase
  def test_get_rubygems_data
    lockfilegems = LockfileGemsTest.new
    lockfilegems.execute
+   assert(lockfilegems.result.length == 2)
    result = lockfilegems.result["test"].name
    assert_equal("test",result)
    result = lockfilegems.result["test"].version
    assert_equal(Gem::Version.new("0.8.6"), result)
+   result = lockfilegems.result["test2"].name
+   assert_equal("test2",result)
+   result = lockfilegems.result["test2"].version
+   assert_equal(Gem::Version.new("1.2.3"), result)
  end
 end
 
