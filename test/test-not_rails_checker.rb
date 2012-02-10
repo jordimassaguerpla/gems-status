5acfd05af2e3c5592368469bc87744f436134861
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2012-02-10 19:35:18 +0100

    added test no rails checker

---
+++ test/test-not_rails_checker.rb
@@ -0,0 +1,54 @@
+$:.unshift File.join(File.dirname(__FILE__), "..", "lib")
+require 'rubygems'
+require 'test/unit'
+require 'gems_command'
+require 'gem_simple'
+require 'not_rails_checker'
+
+class TestNotRailsChecker < Test::Unit::TestCase
+  def test_gem_with_no_deps
+    gem = GemSimple.new("name", Gem::Version.new("1.2.3"), nil, nil, nil, nil)
+    check = NotRailsChecker
+    result = check.check?(gem)
+    assert(!result)
+  end
+  def test_gem_with_no_deps_2
+    gem = GemSimple.new("name", Gem::Version.new("1.2.3"), nil, nil, nil, [])
+    check = NotRailsChecker
+    result = check.check?(gem)
+    assert(result)
+  end
+  def test_gem_with_rail_dep
+    deps = [
+      Gem::Dependency.new("rails", Gem::Requirement.new("> 0.0.0")),
+      Gem::Dependency.new("foo", Gem::Requirement.new("> 0.0.0"))
+    ]
+
+    gem = GemSimple.new("name", Gem::Version.new("1.2.3"), nil, nil, nil, deps)
+    check = NotRailsChecker
+    result = check.check?(gem)
+    assert(!result)
+  end
+  def test_gem_with_no_rail_dep
+    deps = [
+      Gem::Dependency.new("norails", Gem::Requirement.new("> 0.0.0")),
+      Gem::Dependency.new("foo", Gem::Requirement.new("> 0.0.0"))
+    ]
+
+    gem = GemSimple.new("name", Gem::Version.new("1.2.3"), nil, nil, nil, deps)
+    check = NotRailsChecker
+    result = check.check?(gem)
+    assert(result)
+  end
+  def test_gem_with_railties_dep
+    deps = [
+      Gem::Dependency.new("railties", Gem::Requirement.new("> 0.0.0")),
+      Gem::Dependency.new("foo", Gem::Requirement.new("> 0.0.0"))
+    ]
+
+    gem = GemSimple.new("name", Gem::Version.new("1.2.3"), nil, nil, nil, deps)
+    check = NotRailsChecker
+    result = check.check?(gem)
+    assert(!result)
+  end
+end
