bb430335fad41c9afdcee471769b3fe24a3fe030
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2012-06-27 18:36:28 +0200

    add more security keywords

--- lib/gems-status/scm_security_messages.rb
+++ lib/gems-status/scm_security_messages.rb
@@ -1,5 +1,5 @@
 class ScmSecurityMessages
   def check_message?(msg)
-    return msg.include?("security")
+    return msg =~ /XSS|CSRF|cross-site|cross site|crosssite|injection|forgery|traversal|CVE|unsafe|vulnerab|risk/i 
   end
 end
