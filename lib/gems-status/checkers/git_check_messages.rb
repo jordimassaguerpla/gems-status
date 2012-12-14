1301b7908f1097c5170df7595198230e9d33eb5c
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2012-12-14 19:37:04 +0100

    look more log entries
    This fixes the issues that not all git entries were searched as
    git.log method has a default value of 30 as maximum number of entries.
    This commits rises this number to 8688, the maximum git.log accepts.

--- lib/gems-status/checkers/git_check_messages.rb
+++ lib/gems-status/checkers/git_check_messages.rb
@@ -3,6 +3,9 @@ require "git"
 require "gems-status/checkers/scm_check_messages"
 
 class GitCheckMessages < ScmCheckMessages
+  # This value is the maximum log entries that git.log accepts.
+  # It is a magic number found through testing.
+  MAX_NUM_MESSAGES = 8688
 
 private
 
@@ -17,7 +20,7 @@ private
       g = Git.clone(source_repo, name)
     end
     g.pull
-    return g.log
+    return g.log MAX_NUM_MESSAGES
   end
 
   def commit_key(commit)
