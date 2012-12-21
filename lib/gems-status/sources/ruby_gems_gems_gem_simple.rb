5c84ce4765206bee8d351e09895b28e901ea27d9
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2012-12-21 17:37:17 +0100

    add error handling

--- lib/gems-status/sources/ruby_gems_gems_gem_simple.rb
+++ lib/gems-status/sources/ruby_gems_gems_gem_simple.rb
@@ -29,12 +29,16 @@ class RubyGemsGems_GemSimple < GemSimple
 
   def date
     Utils::log_debug "looking for date for #{@name} - #{@version}"
-    versions = JSON.parse(open("https://rubygems.org/api/v1/versions/#{@name}.json").read)
-    versions.each do |version|
-      if Gem::Version.new(version["number"]) == @version
-        Utils::log_debug  "Date for #{@name} - #{@version} : #{version["built_at"]}"
-        return Time.parse version["built_at"]
+    begin
+      versions = JSON.parse(open("https://rubygems.org/api/v1/versions/#{@name}.json").read)
+      versions.each do |version|
+        if Gem::Version.new(version["number"]) == @version
+          Utils::log_debug  "Date for #{@name} - #{@version} : #{version["built_at"]}"
+          return Time.parse version["built_at"]
+        end
       end
+    rescue
+      Utils::log_error(@name, "There was a problem opening https://rubygems.org/api/v1/versions/#{@name}.json")
     end
     nil
   end
