adfa92552ff8894e89f58c15cad2a9aa43c673ad
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2013-01-10 17:49:21 +0100

    get license information from yaml file when it is not informed by the gem

--- lib/gems-status/checkers/print_gem_versions.rb
+++ lib/gems-status/checkers/print_gem_versions.rb
@@ -1,14 +1,32 @@
 class PrintGemVersions
   def initialize(conf)
+    Utils::check_parameters('PrintGemVersions', conf, ["licenses"])
+    begin
+      @licenses = YAML::load(File::open(conf["licenses"]))
+    rescue
+      Utils::log_error("?", "There was a problem opening #{conf["licenses"]}")
+      @licenses = []
+    end
   end
 
   def check?(gem)
     #ignore upstream gems
     return true if gem.origin == gem.gems_url 
     open("gemversions.txt", "a") do |f|
-      f.puts "#{gem.name} #{gem.version} #{gem.license}"
+      if ! (license = gem.license)
+        license = get_license_string( gem.name, gem.version.to_s)
+      end
+      f.puts "#{gem.name} #{gem.version} #{license}"
     end
     return true
+  end
+
+  private
+
+  def get_license_string(name, version)
+    return "" if ! @licenses[name]
+    return "#{@licenses[name][version]}*" if @licenses[name][version]
+    return "#{@licenses[name].sort.last.join("->")}**"
   end
 
 end
--- lib/gems-status/utils.rb
+++ lib/gems-status/utils.rb
@@ -5,7 +5,9 @@ class Utils
   attr_accessor :errors
   @@errors = {}
   @@md5_sums = {}
+  @@licenses = {}
   @@keys = {}
+  @@gems = {}
 
   def Utils.errors
     return @@errors
@@ -35,21 +37,57 @@ class Utils
     $stderr.puts "DEBUG: #{msg}"
   end
 
-  def Utils.download_md5(gem_uri)
-    return @@md5_sums[gem_uri] if @@md5_sums[gem_uri]
+  def Utils.download_md5(name, version, gems_url)
+    key = "#{name}-#{version}-#{gems_url.gsub("/", "_").gsub(":", "_")}"
+    return @@md5_sums[key] if @@md5_sums[key]
     begin
-      if gem_uri.user && gem_uri.password
-        source = open(gem_uri.scheme + "://" + gem_uri.host + "/" + gem_uri.path, 
-                      :http_basic_authentication=>[gem_uri.user, gem_uri.password])
-      else
-        source = open(gem_uri)
-      end
+      gem_file_path = self.download_gem(name, version, gems_url)
     rescue Exception => e
-      self.log_error e.message
+      self.log_error(name, e.message)
+      return nil
     end
-    md5 = Digest::MD5.hexdigest(source.read)
-    @@md5_sums[gem_uri] = md5
+    md5 = Digest::MD5.hexdigest(open(gem_file_path).read)
+    @@md5_sums["#{name}-#{version}"] = md5
     return md5
   end
 
+  def Utils.download_license(name, version, gems_url)
+    key = "#{name}-#{version}-#{gems_url.gsub("/", "_").gsub(":", "_")}"
+    return @@licenses[key] if @@licenses[key]
+    begin
+      gem_file_path = self.download_gem(name, version, gems_url)
+    rescue Exception => e
+      self.log_error(name, e.message)
+      return nil
+    end
+    license = Gem::Format.from_file_by_path(gem_file_path).spec.license
+    @@licenses[key] = license
+    return license
+  end
+
+  private
+
+  def Utils.download_gem(name, version, gems_url)
+    gem_uri = URI.parse("#{gems_url}/#{name}-#{version}.gem")
+    tmp_path = "tmp/utils/gems/#{gems_url.gsub("/", "_").gsub(":", "_")}/"
+    gem_name = "#{name}-#{version}.gem"
+    full_path = "#{tmp_path}/#{gem_name}"
+    return full_path if File.exists? full_path
+    uri_debug = gem_uri.clone
+    uri_debug.password = "********" if uri_debug.password
+    Utils::log_debug "download #{@name} from #{uri_debug}"
+    FileUtils::mkdir_p(tmp_path) if ! File.exists?(tmp_path)
+    if gem_uri.user && gem_uri.password
+      source = open(gem_uri.scheme + "://" + gem_uri.host + "/" + gem_uri.path,
+                   "rb",
+                    :http_basic_authentication=>[gem_uri.user, gem_uri.password])
+    else
+      source = open(gem_uri)
+    end
+    open(full_path, "wb") do |file|
+      file.write(source.read)
+    end
+    source.close
+    return full_path
+  end
 end
