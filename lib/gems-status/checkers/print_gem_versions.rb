af092572241ace449607743020a2b928278a1087
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2013-01-10 16:46:13 +0100

    revert format in the print gem versions file
    our security team expects the previous format. Moreover, the fields
    are obvious enough and adding the labels makes it more unreadable (imho)
    instead of the opposite.

--- lib/gems-status/checkers/print_gem_versions.rb
+++ lib/gems-status/checkers/print_gem_versions.rb
@@ -6,7 +6,7 @@ class PrintGemVersions
     #ignore upstream gems
     return true if gem.origin == gem.gems_url 
     open("gemversions.txt", "a") do |f|
-      f.puts "name:#{gem.name} \tversion:#{gem.version}\t license:#{gem.license}"
+      f.puts "#{gem.name} #{gem.version} #{gem.license}"
     end
     return true
   end
