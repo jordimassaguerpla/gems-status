c7395aa95614583b814f823a8fada03b35b20c90
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2013-02-11 13:14:28 +0100

    add DoS and Malicious keywords to the security checker for commits

--- lib/gems-status/checkers/scm_security_messages.rb
+++ lib/gems-status/checkers/scm_security_messages.rb
@@ -1,5 +1,5 @@
 class ScmSecurityMessages
   def check_message?(msg)
-    return msg =~ /XSS|CSRF|cross-site|cross site|crosssite|injection|forgery|traversal|CVE|unsafe|vulnerab|risk|security/i 
+    return msg =~ /XSS|CSRF|cross-site|cross site|crosssite|injection|forgery|traversal|CVE|unsafe|vulnerab|risk|security|Malicious|DoS/i 
   end
 end
