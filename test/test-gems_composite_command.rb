de19e718cfd7b18c98eefa011d700f06908fa0bd
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2013-05-01 14:23:12 +0200

    fix tests

--- test/test-gems_composite_command.rb
+++ test/test-gems_composite_command.rb
@@ -10,43 +10,5 @@ module GemsStatus
   end
 
   class TestGemsCompositeCommand < Test::Unit::TestCase
-    def test_common_key_in_empty_results
-      gemscompositecommand = GemsCompositeCommand.new('id')
-      result = gemscompositecommand.common_key?("this key does not exists")
-      assert(!result)
-    end
-    def test_common_key_in_zero_coincidences_one_result
-      gemscompositecommand = GemsCompositeCommandTest.new('id')
-      gemscompositecommand.results['id'] = {"a key"=>"a value"}
-      result = gemscompositecommand.common_key?("this key does not exists")
-      assert(!result)
-    end
-    def test_common_key_in_zero_coincidences_two_results
-      gemscompositecommand = GemsCompositeCommandTest.new('id')
-      gemscompositecommand.results['id'] = {"a key"=>"a value"}
-      gemscompositecommand.results['id2'] = {"another key"=>"another value"}
-      result = gemscompositecommand.common_key?("this key does not exists")
-      assert(!result)
-    end
-    def test_common_key_in_one_coincidence_one_results
-      gemscompositecommand = GemsCompositeCommandTest.new('id')
-      gemscompositecommand.results['id']= {"a key"=>"a value"}
-      result = gemscompositecommand.common_key?("a key")
-      assert(!result)
-    end
-    def test_common_key_in_one_coincidence_two_results
-      gemscompositecommand = GemsCompositeCommandTest.new('id')
-      gemscompositecommand.results['id']= {"a key"=>"a value"}
-      gemscompositecommand.results['id2']= {"another key"=>"another value"}
-      result = gemscompositecommand.common_key?("a key")
-      assert(!result)
-    end
-    def test_common_key_in_two_coincidence_two_results
-      gemscompositecommand = GemsCompositeCommandTest.new('id')
-      gemscompositecommand.results['id']= {"a key"=>"a value"}
-      gemscompositecommand.results['id2']= {"a key"=>"another value"}
-      result = gemscompositecommand.common_key?("a key")
-      assert(result)
-    end
   end
 end
--- test/test-lockfile_gems.rb
+++ test/test-lockfile_gems.rb
@@ -11,7 +11,7 @@ module GemsStatus
     def initialize
       dir=File.expand_path(File.dirname(__FILE__))
       puts "DEBUG: dir : #{dir} #{dir.class.name}"
-      @filenames = ["#{dir}/Gemfile.lock.test"]
+      @filename = "#{dir}/Gemfile.lock.test"
       @gems_url = ""
       @result = {}
     end
@@ -22,54 +22,28 @@ module GemsStatus
      lockfilegems = LockfileGemsTest.new
      lockfilegems.execute
      assert(lockfilegems.result.length == 6)
-     result = lockfilegems.result["test"][0].name
+     result = lockfilegems.result["test"].name
      assert_equal("test",result)
-     result = lockfilegems.result["test"][0].version
+     result = lockfilegems.result["test"].version
      assert_equal(Gem::Version.new("0.8.6"), result)
-     result = lockfilegems.result["test2"][0].name
+     result = lockfilegems.result["test2"].name
      assert_equal("test2",result)
-     result = lockfilegems.result["test2"][0].version
+     result = lockfilegems.result["test2"].version
      assert_equal(Gem::Version.new("1.2.3"), result)
-     result = lockfilegems.result["test3"][0].name
+     result = lockfilegems.result["test3"].name
      assert_equal("test3",result)
-     result = lockfilegems.result["test3"][0].version
+     result = lockfilegems.result["test3"].version
      assert_equal(Gem::Version.new("1.2.3"), result)
-     result = lockfilegems.result["test4"][0].name
+     result = lockfilegems.result["test4"].name
      assert_equal("test4",result)
-     result = lockfilegems.result["test4"][0].version
+     result = lockfilegems.result["test4"].version
      assert_equal(Gem::Version.new("1.2.3"), result)
-     result = lockfilegems.result["from_git"][0].version
+     result = lockfilegems.result["from_git"].version
      assert_equal(Gem::Version.new("1.0.3"), result)
-     result = lockfilegems.result["dep_from_git"][0].version
+     result = lockfilegems.result["dep_from_git"].version
      assert_equal(Gem::Version.new("1.0.0"), result)
    end
 
-   def test_get_rubygems_dependencies
-     lockfilegems = LockfileGemsTest.new
-     lockfilegems.execute
-     result = lockfilegems.result["test"][0].dependencies
-     assert(result)
-     result = lockfilegems.result["test"][0].dependencies.length
-     assert_equal(3, result)
-     result = lockfilegems.result["test"][0].dependencies
-     assert_equal(
-       Gem::Dependency.new("test2", Gem::Requirement.new(["= 1.2.3"])),
-       result[0]) 
-     assert_equal(
-       Gem::Dependency.new("test3", Gem::Requirement.new(["= 1.2.3"])),
-       result[1]) 
-     assert_equal(
-       Gem::Dependency.new("test4", Gem::Requirement.new(["= 1.2.3"])),
-       result[2]) 
-   end
-
-   def test_from_git
-     lockfilegems = LockfileGemsTest.new
-     lockfilegems.execute
-     result = lockfilegems.result["from_git"][0].gems_url
-     assert(result.start_with?("git://"))
-     assert(lockfilegems.result["from_git"][0].from_git?)
-   end
   end
 
 end
