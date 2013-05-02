f99b4b91598674aeeaefa8a2ca657044f785170f
Jordi Massaguer Pla <jordimassaguerpla@gmail.com>
2013-05-02 23:58:24 +0200

    make attributes readable in is_rubygems so that we can read them ...
    in the gems-status-web

--- lib/gems-status/checkers/is_rubygems.rb
+++ lib/gems-status/checkers/is_rubygems.rb
@@ -6,9 +6,10 @@ require 'gems-status/utils'
 module GemsStatus
 
   class IsRubygems < GemChecker
+    attr_reader :rubygems_md5, :gem_md5
 
     def initialize(configuration)
-      @md5 = nil
+      @rubygems_md5 = nil
       @gem_md5 = nil
       super configuration
     end
@@ -17,18 +18,18 @@ module GemsStatus
       Utils::log_debug("Looking for #{gem.name}")
       result = nil
       gem_uri = "http://rubygems.org/downloads/#{gem.name}-#{gem.version}.gem" 
-      @md5 = Utils::download_md5(gem.name, gem.version, "http://rubygems.org/downloads")
+      @rubygems_md5 = Utils::download_md5(gem.name, gem.version, "http://rubygems.org/downloads")
       @gem_md5 = gem.md5
-      !@md5 && !@gem_md5 && @gem_md5== @md5
+      !@rubygems_md5 && !@gem_md5 && @gem_md5== @rubygems_md5
     end
     
     def description
-      if !@md5
+      if !@rubygems_md5
         "This gem does not exist in rubygems.org "
       elsif !@gem_md5
         "This gem does not exist in your server"
-      elsif @md5 != @gem_md5
-        "This gem has a different md5sum than in rubygems.org\nrubygems: #{@md5} your server #{@gem_md5}"
+      elsif @rubygems_md5 != @gem_md5
+        "This gem has a different md5sum than in rubygems.org\nrubygems: #{@rubygems_md5} your server #{@gem_md5}"
       end
     end
   end
