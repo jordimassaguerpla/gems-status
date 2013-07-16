e72bf4dfff7240a0e2064d76ef5d5b29e1569c44
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2013-07-16 18:30:05 +0200

    a test file that I had forgotten

---
+++ test/test-not_a_security_alert_checker.rb
@@ -0,0 +1,20 @@
+require './test/test-helper.rb'
+$:.unshift File.join(File.dirname(__FILE__), "..", "lib")
+require 'test/unit'
+require 'gems-status'
+
+module GemsStatus
+  class NotASecurityAlertChecker
+    def initialize(conf)
+    end
+  end
+  class NotASecurityAlertCheckerTest <Test::Unit::TestCase 
+    def test_match_name
+      ch = NotASecurityAlertChecker.new([])
+      assert ch.match_name("rubygem mail", "mail")
+      assert !ch.match_name("mail","mail")
+      assert ch.match_name("ruby mail", "mail")
+      assert ch.match_name("mail gem", "mail")
+    end
+  end
+end
