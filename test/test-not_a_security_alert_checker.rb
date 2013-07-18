98b5908a966777735851c986183aad24b9d020ab
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2013-07-18 16:43:14 +0200

    increase coverage to 70.8%

--- test/test-not_a_security_alert_checker.rb
+++ test/test-not_a_security_alert_checker.rb
@@ -5,9 +5,32 @@ require 'gems-status'
 
 module GemsStatus
   class NotASecurityAlertChecker
+    attr_reader :security_messages, :fixed
+    attr_accessor :emails
     def initialize(conf)
+      @security_messages = {}
+      @fixed = {}
     end
-    public :match_name
+    public :match_name, :key_for_emails, :look_in_emails, :gem_uri, :filter_security_messages_already_fixed
+  end
+  class MockGem
+    def name
+      "NAME"
+    end
+    def origin
+      "ORG"
+    end
+    def date
+      Date.new(2012, 12, 12)
+    end
+  end
+  class MockEmail
+    def uid
+      "UID"
+    end
+    def subject
+      "subject"
+    end
   end
   class NotASecurityAlertCheckerTest <Test::Unit::TestCase 
     def test_match_name
@@ -17,5 +40,119 @@ module GemsStatus
       assert ch.match_name("ruby mail", "mail")
       assert ch.match_name("mail gem", "mail")
     end
+
+    def test_key_for_emails
+     ch = NotASecurityAlertChecker.new([])
+     result = ch.key_for_emails( "LN", MockGem.new, MockEmail.new)
+      assert_equal "email_LN_NAME_ORG_UID", result
+    end
+
+    def test_look_in_emails_for_rubyonrails_sec_mail
+      ch  = NotASecurityAlertChecker.new([])
+      ch.emails = {
+        "rubyonrails-security@googlegroups.com" => [MockEmail.new]
+      }
+      gem = MockGem.new
+      def gem.name
+        "rails"
+      end
+      assert_equal Hash, ch.security_messages.class
+      assert_equal 0, ch.security_messages.length
+      ch.look_in_emails(gem)
+      assert_equal Hash, ch.security_messages.class
+      assert_equal 1, ch.security_messages.length
+    end
+
+    def test_look_in_emails_for_other_mail
+      mail = MockEmail.new
+      def mail.subject
+        "gem rails"
+      end
+      ch  = NotASecurityAlertChecker.new([])
+      ch.emails = {
+        "other" => [mail]
+      }
+      gem = MockGem.new
+      def gem.name
+        "rails"
+      end
+      assert_equal Hash, ch.security_messages.class
+      assert_equal 0, ch.security_messages.length
+      ch.look_in_emails(gem)
+      assert_equal Hash, ch.security_messages.class
+      assert_equal 1, ch.security_messages.length
+    end
+
+    def test_gem_uri_with_project_uri
+      ch = NotASecurityAlertChecker.new([])
+
+      result = ch.gem_uri({"project_uri" => "github.com/a"})
+      assert_equal "github.com/a", result
+      result = ch.gem_uri({"project_uri" => "a"})
+      assert_equal nil, result
+    end
+
+    def test_gem_uri_with_homepage_url
+      ch = NotASecurityAlertChecker.new([])
+
+      result = ch.gem_uri({"homepage_uri" => "github.com/a"})
+      assert_equal "github.com/a", result
+      result = ch.gem_uri({"homepage_uri" => "a"})
+      assert_equal nil, result
+    end
+
+    def test_gem_uri_with_source_code_uri
+      ch = NotASecurityAlertChecker.new([])
+
+      result = ch.gem_uri({"source_code_uri" => "github.com/a"})
+      assert_equal "github.com/a", result
+      result = ch.gem_uri({"source_code_uri" => "a"})
+      assert_equal nil, result
+    end
+
+    def test_filter_security_messages_already_fixed_with_equal_version
+      ch = NotASecurityAlertChecker.new([])
+      gem = MockGem.new
+      ch.security_messages["key"] = gem
+      ch.fixed["key"] = "1.1.1"
+      version = Gem::Version.new("1.1.1")
+      date = Date.new(2011, 12, 12)
+      ch.filter_security_messages_already_fixed(version, date)
+      assert_equal 0, ch.security_messages.length
+    end
+
+    def test_filter_security_messages_already_fixed_with_newer_version
+      ch = NotASecurityAlertChecker.new([])
+      gem = MockGem.new
+      ch.security_messages["key"] = gem
+      ch.fixed["key"] = "1.1.0"
+      version = Gem::Version.new("1.1.1")
+      date = Date.new(2011, 12, 12)
+      ch.filter_security_messages_already_fixed(version, date)
+      assert_equal 0, ch.security_messages.length
+    end
+
+    def test_filter_security_messages_already_fixed_with_older_version
+      ch = NotASecurityAlertChecker.new([])
+      gem = MockGem.new
+      ch.security_messages["key"] = gem
+      ch.fixed["key"] = "1.1.2"
+      version = Gem::Version.new("1.1.1")
+      date = Date.new(2011, 12, 12)
+      ch.filter_security_messages_already_fixed(version, date)
+      assert_equal 1, ch.security_messages.length
+    end
+
+    def test_filter_security_messages_already_fixed_with_newer_date
+      ch = NotASecurityAlertChecker.new([])
+      gem = MockGem.new
+      ch.security_messages["key"] = gem
+      ch.fixed["key"] = "1.1.2"
+      version = Gem::Version.new("1.1.1")
+      date = Date.new(2013, 12, 12)
+      ch.filter_security_messages_already_fixed(version, date)
+      assert_equal 0, ch.security_messages.length
+    end
+
   end
 end
